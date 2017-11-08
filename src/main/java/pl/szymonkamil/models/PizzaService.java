f81bcc5f2bf9656861c21d7a82b93ceed5cb7420
Szymon Chowaniec <szymonchowaniec@gmail.com>
2017-11-08 16:14:34 +0100

    add pizza service

--- src/main/java/pl/szymonkamil/models/PizzaService.java
+++ src/main/java/pl/szymonkamil/models/PizzaService.java
@@ -1,4 +1,27 @@
 package pl.szymonkamil.models;
 
+import org.json.JSONObject;
+import pl.szymonkamil.models.utils.Config;
+import pl.szymonkamil.models.utils.HttpUtils;
+
 public class PizzaService {
+
+
+    void makeCall( String city){
+
+        parseJsonData(HttpUtils.makeHttpRequest(Config.APP_URL+city+"@key="+Config.APP_ID));
+
+    }
+
+    private void parseJsonData(String text) {
+
+        JSONObject root = new JSONObject(text);
+
+        JSONObject main = root.getJSONObject("main");
+
+
+
+
+
+    }
 }
