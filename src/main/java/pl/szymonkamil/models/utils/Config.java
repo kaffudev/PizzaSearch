4dfe15dcf81f4b65e7a4c535a067749b5f4afe04
kaffudev <kaffudev@gmail.com>
2017-11-08 16:03:10 +0100

    added config

--- src/main/java/pl/szymonkamil/models/utils/Config.java
+++ src/main/java/pl/szymonkamil/models/utils/Config.java
@@ -2,4 +2,8 @@ package pl.szymonkamil.models.utils;
 
 public class Config {
 
+        public static final String APP_ID = "AIzaSyAX40A9IFQt4kOgAu9sE-vXDpPTmqBVKc8";
+        public static final String APP_URL =
+                "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
+
 }
--- src/main/java/pl/szymonkamil/models/utils/HttpUtils.java
+++ src/main/java/pl/szymonkamil/models/utils/HttpUtils.java
@@ -1,4 +1,19 @@
 package pl.szymonkamil.models.utils;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class HttpUtils {
+    public static String makeHttpRequest(String url){
+
+        try {
+            URL urlClass = new URL(url);
+
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
